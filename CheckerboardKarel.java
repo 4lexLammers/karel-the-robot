8080290fbf1b976fb2463fd4a78b3a95de5423fd
Alexander Lammers <4lexlammers@outlook.com>
2016-07-22 00:25:41 +0200

    Add CheckerboardKarel.java

--- CheckerboardKarel.java
+++ CheckerboardKarel.java
@@ -3,67 +3,84 @@
  * ----------------------------
  * CheckerboardKarel class draws a checkerboard using beepers.
  * This works for all sizes of the world.
+ *
+ * Code by Alexander Lammers
  */
 
 import stanford.karel.*;
 
 public class CheckerboardKarel extends SuperKarel {
 
-	// not working
 	public void run() {
-		if (frontIsBlocked()){
+		if (frontIsBlocked()) {
 			turnLeft();
 			fillRow();
 		}
 			while (frontIsClear()) {
 				fillRow();
-					if (frontIsBlocked()){
-						goToNextRow();
-					}			
+				if (frontIsBlocked()) {
+					goToNextRow();
+				}			
 			}
 	}
 
+	/*
+	* This method fills the current row with beepers
+	* Pre-condition: Karel is facing east or west
+	* Post-condition: Karel stands in front of a wall
+	*/
 		
 	private void fillRow() {
 		putBeeper();
 		move();
-			if (frontIsClear()){
-				move();
-					// If the number of avenues is odd
-					if(frontIsBlocked()) {
-						putBeeper();
-					}
+		if (frontIsClear()) {
+			move();
+
+				/*
+				* If the number of avenues is odd
+				*/ 
+
+			if(frontIsBlocked()) {
+				putBeeper();
 			}
+		}
 
 	}
-	// Pre-Condition: Karel needs to face east or west
-	// Post-Condition: 
+
+	/*
+	* This method tells Karel to go to the next row
+	* Pre-condition: Karel stands in front of a wall 
+	* 	and is facing east or west
+	* Post-condition: Karel is facing east or west on the next row
+	*/
+
 	private void goToNextRow() {
-		if (facingEast() & beepersPresent()){
+		if (facingEast() & beepersPresent()) {
 			turnLeft();
-				if (frontIsClear()){
-					move();
-					turnLeft();
-					move();
-				}
-		} 
-		else if (facingEast() & noBeepersPresent()){
-			turnLeft();
-				if (frontIsClear()){
-					move();
-					turnLeft();
+			if (frontIsClear()) {
+				move();
+				turnLeft();
+				move();
 			}
 		} 
-		else if (facingWest() & beepersPresent()){
+		else if (facingEast() & noBeepersPresent()) {
+			turnLeft();
+			if (frontIsClear()) {
+				move();
+				turnLeft();
+			}
+		} 
+		else if (facingWest() & beepersPresent()) {
 			turnRight();
-				if (frontIsClear()){
-					move();
-					turnRight();
-					move();
-				}
+			if (frontIsClear()) {
+				move();
+				turnRight();
+				move();				
+			}
+
 		} else {
 			turnRight();
-			if (frontIsClear()){
+			if (frontIsClear()) {
 				move();
 				turnRight();
 			}
